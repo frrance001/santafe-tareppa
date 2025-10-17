f544cb858d5f64f2bba7e5890cd25bf95631d4a3
frrance001 <franzedez@gmail.com>
2025-10-17 22:16:21 +0800

    has

--- routes/web.php
+++ routes/web.php
@@ -273,3 +273,7 @@ Route::get('/passenger/payment/failed', [PaymentController::class, 'failed'])->n
 Route::delete('/passenger/ride/{id}', [RideController::class, 'destroy'])
     ->name('passenger.ride.destroy');
 
+use App\Http\Controllers\Auth\OtpLoginController;
+
+Route::post('/send-otp', [OtpLoginController::class, 'sendOtp'])->name('send.otp');
+Route::post('/verify-otp', [OtpLoginController::class, 'verifyOtp'])->name('verify.otp');
