2f09ec45dd0f6f6740dbeb494208f170e9cbd6d2
frrance001 <franzedez@gmail.com>
2025-11-26 14:33:52 +0800

    As

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,7 +11,7 @@ class AdminController extends Controller
     public function index()
     {
         $users = User::all()->groupBy('role');
-        return view('admin.users.index', compact('users'));
+        return view('admin.user.index', compact('users'));
     }
 
     // Approve user
